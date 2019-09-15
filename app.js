ac649950186a97a218b661869ae4683c4468cf6d
Joseph Chauvin <joseph.chauvin102@gmail.com>
2019-09-15 08:13:47 -0500

    Working Working

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const data = require('data.json');
+// app.set('view engine', 'pug');
+// app.use(express.static('public'));
+
+// app.get('/', (req, res) => {
+//     const data = data.projects;
+//     app.render('home', {data})
+// });
+
+// app.get('/about', (req, res) => {
+//     app.render('about');
+// });
+
+// app.get('/projects', (req, res) => {
+//     const id = data.projects.id;
+//     app.render(`/`, {data});
+// });
+
+app.listen(3000, console.log());
