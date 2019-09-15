88a08e38ddb7a5d7e30a5c3c0df5b7f57fc70a95
Joseph Chauvin <joseph.chauvin102@gmail.com>
2019-09-15 08:46:48 -0500

    Fixed data file path

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
-const data = require('data.json');
-// app.set('view engine', 'pug');
-// app.use(express.static('public'));
+const data = require('./data.json');
+app.set('view engine', 'pug');
+app.use(express.static('public'));
 
-// app.get('/', (req, res) => {
-//     const data = data.projects;
-//     app.render('home', {data})
-// });
+app.get('/', (req, res) => {
+    app.render('home', {data});
+});
 
 // app.get('/about', (req, res) => {
 //     app.render('about');
@@ -18,4 +17,4 @@ const data = require('data.json');
 //     app.render(`/`, {data});
 // });
 
-app.listen(3000, console.log());
+app.listen(3000, console.log('This server is running on port 3000!'));
