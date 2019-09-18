ef691a094b39991dc46db1ec2b47a1d7965c8812
Joseph Chauvin <joseph.chauvin102@gmail.com>
2019-09-17 20:26:05 -0500

    finsihed project ?

--- app.js
+++ app.js
@@ -1,20 +1,65 @@
+
+
+//all required files, libraries, etc...
 const express = require('express');
 const app = express();
 const data = require('./data.json');
+const projects = data.projects;
 app.set('view engine', 'pug');
-app.use(express.static('public'));
+app.use('/static', express.static('public'));
 
+
+//renders home page template
 app.get('/', (req, res) => {
-    app.render('home', {data});
+    res.render('index', {projects});
 });
 
-// app.get('/about', (req, res) => {
-//     app.render('about');
-// });
 
-// app.get('/projects', (req, res) => {
-//     const id = data.projects.id;
-//     app.render(`/`, {data});
-// });
+//renders about page template
+app.get('/about', (req, res) => {
+    res.render('about', {projects});
+});
 
+
+//if user types in '/project' it directs them to the first project
+app.get('/project', (req, res) => {
+    res.redirect('/project/0');
+});
+
+
+//renders each project page based on pug template
+app.get('/project/:id', (req, res, next) => {
+    const { id } = req.params;
+    if ( (id >= 0 && id <= 4) && isNaN(id) === false) {
+        const project = projects[id];
+        res.render('project', {project, id}); 
+    } else {
+        next();
+    }
+});
+
+
+// Creating new error
+app.use((req, res, next) => {
+    const err = new Error('There has been an error!');
+    err.status = 404;
+    next(err);
+});
+
+
+//using that error to display error pug template
+app.use((err, req, res, next) => {
+    res.status(err.status);
+    err.message = 'There has been an error';
+    res.locals.error = err;
+    if (res.status = 404) {
+        console.log( `${req.path} is not a valid path`);
+    }
+    res.render('error'); 
+});
+
+
+//app is available on localhost:3000
 app.listen(3000, console.log('This server is running on port 3000!'));
+
+
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,26 @@
-$(document).foundation()
+$(document).foundation();
+
+function isVisable(section) {
+  let sectionRec = section.getBoundingClientRect();
+  let distanceFromTop = -200;
+
+  if (sectionRec.top - window.innerHeight < distanceFromTop) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function scanDocument() {
+  let hiddenElements = document.querySelectorAll('.hidden');
+
+  for (let i = 0; i < hiddenElements.length; i += 1) {
+    if (isVisable(hiddenElements[i])) {
+      hiddenElements[i].classList.remove('hidden');
+      hiddenElements[i].style.setProperty('animation-name', 'fadeInMoveUp');
+      hiddenElements[i].style.setProperty('animation-duration', '.8s');
+    }
+  }
+}
+
+scanDocument();
